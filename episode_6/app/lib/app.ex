c882f9522938d95cdaa4eda79ae251f7578cd7ff
Doug Wright <mrdougwright@gmail.com>
2019-08-05 12:51:37 -0700

    episode 6

---
+++ episode_6/app/lib/app.ex
@@ -0,0 +1,22 @@
+defmodule App do
+  @moduledoc """
+  Documentation for App.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> App.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def hi do
+    :howdy
+  end
+end
