eac6fd9192c691c55371f65cde873b5f50e6af7d
Doug Wright <mrdougwright@gmail.com>
2019-09-11 09:49:29 -0700

    episode 12

---
+++ episode_12/rps.exs
@@ -0,0 +1,28 @@
+# Rock, Paper, Scissors
+# players = letters, i.e. "RPSPRS"
+# answer -> P, S, R
+
+defmodule RPS do
+  @winners %{"R" => "S", "S" => "P", "P" => "R"}
+
+  def find_winners(players) do
+    chars = String.split(players, "", trim: true)
+
+    get_winners(chars)
+  end
+
+  def get_winners([p1, p2 | rest]) do
+    find_winner(p1, p2) ++ get_winners(rest)
+  end
+
+  def get_winners(other), do: other
+
+  def find_winner(p1, p1), do: []
+
+  def find_winner(p1, p2) do
+    case @winners[p1] == p2 do
+      true -> [p1]
+      false -> [p2]
+    end
+  end
+end
