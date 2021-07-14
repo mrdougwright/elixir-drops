5ffe777baf68af838292042685d2c4fed5e862f0
Doug Wright <mrdougwright@gmail.com>
2021-07-14 12:51:38 -0500

    Top 5 Elixir Functions

--- episode_05/functions.ex
+++ episode_05/functions.ex
@@ -13,7 +13,6 @@ nil
 Map.get(%{a: "ayeee"}, :b, "beee")
 "beee"
 
-
 # Map.put
 Map.put(%{a: "ayeee"}, :b, "beee")
 %{a: "ayeee", b: "beee"}
@@ -22,31 +21,30 @@ map = %{a: "a"}
 Map.put(map, :b, "b")
 map
 
-
 # Enum.find
-Enum.find([2,4,6], 1)
-#err
-Enum.find([2,4,6], fn(num) -> num == 1 end)
+Enum.find([2, 4, 6], 1)
+# err
+Enum.find([2, 4, 6], fn num -> num == 1 end)
 nil
-Enum.find([2,4,6], 1, fn(num) -> num == 1 end)
+Enum.find([2, 4, 6], 1, fn num -> num == 1 end)
 1
-Enum.find([2,4,6], fn(num) -> num == 2 end)
+Enum.find([2, 4, 6], fn num -> num == 2 end)
 2
-Enum.find([2,4,6], & &1 == 2)
+Enum.find([2, 4, 6], &(&1 == 2))
 2
-
 
 # Enum.reduce
-Enum.reduce([1,2,3], fn(num, acc) -> num * acc end)
-Enum.reduce([1,2,3], & &1 * &2)
+Enum.reduce([1, 2, 3], fn num, acc -> num * acc end)
+Enum.reduce([1, 2, 3], &(&1 * &2))
 6
 
-Enum.reduce([2,4,6], fn(num, acc) -> (num/2) + acc end)
+Enum.reduce([2, 4, 6], fn num, acc -> num / 2 + acc end)
 7.0
+
 # 4/2 + 2 = 4
 # 6/2 + 4 = 7
 
-Enum.reduce([2,4,6], 2, fn(num, acc) -> (num/2) + acc end)
+Enum.reduce([2, 4, 6], 2, fn num, acc -> num / 2 + acc end)
 8.0
 # 2/2 + 2 = 3.0
 # 4/2 + 3 = 5.0
