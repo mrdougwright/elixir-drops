90826a55d4beb15e07a317fee02f44952bbd7988
Doug Wright <mrdougwright@gmail.com>
2021-07-14 12:50:59 -0500

    Elixir, With, Pipes!

--- episode_04/app/lib/manager.ex
+++ episode_04/app/lib/manager.ex
@@ -1,5 +1,4 @@
 defmodule Manager do
-
   def setup_user(params) do
     params
     |> User.create()
@@ -9,11 +8,11 @@ defmodule Manager do
 
   def setup_user(params) do
     with {:ok, %{email: email} = user} <- Ekto.save_user(params),
-      {:ok, msg1} <- SentGrid.send_email(email),
-      {:ok, msg2} <- Trial.start(user) do
-        IO.puts msg1
-        IO.puts msg2
-        user
-      end
+         {:ok, msg1} <- SentGrid.send_email(email),
+         {:ok, msg2} <- Trial.start(user) do
+      IO.puts(msg1)
+      IO.puts(msg2)
+      user
+    end
   end
 end
