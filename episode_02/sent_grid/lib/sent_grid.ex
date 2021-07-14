964f958fd1379175fda5de708d18f4c5a49eb33a
Doug Wright <mrdougwright@gmail.com>
2021-07-14 12:48:41 -0500

    Pattern Matching in Elixir

--- episode_02/sent_grid/lib/sent_grid.ex
+++ episode_02/sent_grid/lib/sent_grid.ex
@@ -1,6 +1,6 @@
 defmodule SentGrid do
   @moduledoc """
-  Documentation for SentGrid.
+  Documentation for SentGrid
   """
 
   @doc """
@@ -16,6 +16,7 @@ defmodule SentGrid do
   def send_email(:marketing) do
     {:ok, "Marketing email sent."}
   end
+
   def send_email(:support), do: {:ok, "Support email sent."}
   def send_email(:other), do: {:ok, "Other email sent."}
   def send_email(_), do: {:error, "Invalid email type."}
